4f105969f90b2d3bf3e94e5868c07e8aef8d8990
David Phillips <david@acz.org>
2016-10-17 15:15:26 -0700

    Remove custom format method from Preconditions
    Now that all the usages are internal to the class and can be easily
    checked statically, there is no need for the custom formatting code.

--- src/main/java/io/airlift/slice/Preconditions.java
+++ src/main/java/io/airlift/slice/Preconditions.java
@@ -17,6 +17,8 @@ package io.airlift.slice;
 
 import javax.annotation.Nullable;
 
+import static java.lang.String.format;
+
 // forked from com.google.common.base.Preconditions
 final class Preconditions
 {
@@ -98,38 +100,5 @@ final class Preconditions
         }
         // end < start
         return format("end index (%s) must not be less than start index (%s)", end, start);
-    }
-
-    private static String format(String template, Object... args)
-    {
-        template = String.valueOf(template); // null -> "null"
-
-        // start substituting the arguments into the '%s' placeholders
-        StringBuilder builder = new StringBuilder(template.length() + 16 * args.length);
-        int templateStart = 0;
-        int i = 0;
-        while (i < args.length) {
-            int placeholderStart = template.indexOf("%s", templateStart);
-            if (placeholderStart == -1) {
-                break;
-            }
-            builder.append(template.substring(templateStart, placeholderStart));
-            builder.append(args[i++]);
-            templateStart = placeholderStart + 2;
-        }
-        builder.append(template.substring(templateStart));
-
-        // if we run out of placeholders, append the extra args in square braces
-        if (i < args.length) {
-            builder.append(" [");
-            builder.append(args[i++]);
-            while (i < args.length) {
-                builder.append(", ");
-                builder.append(args[i++]);
-            }
-            builder.append(']');
-        }
-
-        return builder.toString();
     }
 }
