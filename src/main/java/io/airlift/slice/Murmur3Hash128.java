35b74fc493d673db6feb3c78978d8ab13cb13383
David Phillips <david@acz.org>
2018-01-01 23:32:33 -0800

    Sort modifiers

--- src/main/java/io/airlift/slice/Murmur3Hash128.java
+++ src/main/java/io/airlift/slice/Murmur3Hash128.java
@@ -20,7 +20,7 @@ public final class Murmur3Hash128
     private static final long C1 = 0x87c37b91114253d5L;
     private static final long C2 = 0x4cf5ad432745937fL;
 
-    private final static long DEFAULT_SEED = 0;
+    private static final long DEFAULT_SEED = 0;
 
     private Murmur3Hash128() {}
 
